da0e8cbfe9ffa12e750fd336c5a787e9378b5557
GIL32610 <gil.00000032610@unicap.br>
2022-10-10 10:02:29 -0300

    insertLast

--- Queue/src/br/unicap/edi/ana/Queue.java
+++ Queue/src/br/unicap/edi/ana/Queue.java
@@ -1,21 +1,28 @@
 package br.unicap.edi.ana;
 
 public class Queue<T> {
-    private LSENode<T> first;
-    private LSENode<T> last;
+    private LSENode<T> head;
+    private LSENode<T> tail;
     private int nodes;
-    
+
     public boolean isEmpty() {
-        if (this.first == null && this.last == null && this.nodes == 0) {
+        if (this.head == null && this.tail == null && this.nodes == 0) {
             return true;
         }
         return false;
     }
-    
-    public void insertLast(T content){
-        LSENode<T> node = new LSENode(content);
-    } 
 
-    
-    
+    public void insertLast(T content) {
+        LSENode<T> node = new LSENode(content);
+        if (this.isEmpty()) {
+            this.head = node;
+            this.tail = node;
+            this.nodes++;
+        } else {
+            this.tail.setNext(node);
+            this.tail = node;
+            this.nodes++;
+        }
+    }
+
 }
