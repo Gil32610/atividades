d5236db3eac30bec922effe923afd74778954f74
Hamilton Gomes <hamilton.gomes8@hotmail.com>
2022-10-10 08:52:58 -0300

    Queue is the actual linked list

--- Queue/src/br/unicap/edi/ana/LSEDesc.java
+++ Queue/src/br/unicap/edi/ana/LSEDesc.java
@@ -1,5 +1,18 @@
 package br.unicap.edi.ana;
 
-public class LSEDesc {
+public class LSEDesc<T> {
+    private LSENode<T> first;
+    private LSENode<T> last;
+    private int nodes;
+    
+    public boolean isEmpty() {
+        if (this.first == null && this.last == null && this.nodes == 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    public void insertLast(T cio) 
+    
     
 }
