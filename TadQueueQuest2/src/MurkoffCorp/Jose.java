fe849f418961af4154d9807cda0a929f81a818cf
GIL <gil.00000032610@unicap.br>
2022-10-10 19:24:55 -0300

    Jose

--- TadQueueQuest2/src/MurkoffCorp/Jose.java
+++ TadQueueQuest2/src/MurkoffCorp/Jose.java
@@ -1,5 +1,8 @@
 package MurkoffCorp;
-
+import java.util.Scanner;
 public class Jose {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        
+    }
 }
--- TadQueueQuest2/src/MurkoffCorp/Projeto.java
+++ TadQueueQuest2/src/MurkoffCorp/Projeto.java
@@ -1,5 +1,46 @@
 package MurkoffCorp;
 
 public class Projeto {
+    private String ideia;
+    private String titulo;
+
+    public Projeto(String ideia, String titulo) {
+        this.ideia = ideia;
+        this.titulo = titulo;
+    }
+
+    public Projeto(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIdeia() {
+        return ideia;
+    }
+
+    public void setIdeia(String ideia) {
+        this.ideia = ideia;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
     
+
+    @Override
+    public String toString() {
+        return "Projeto [ideia= " + ideia + ", titulo= " + titulo + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Projeto) {
+            Projeto title = (Projeto) obj;
+            return this.titulo.equals(title.titulo);
+        }
+        return false;
+    }
 }
