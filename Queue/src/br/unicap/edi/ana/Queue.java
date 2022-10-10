776aae16b31449f8706e0b9ce7688721d8ea4fd4
Hamilton Gomes <hamilton.gomes8@hotmail.com>
2022-10-10 09:03:48 -0300

    Queue

--- Queue/src/br/unicap/edi/ana/Queue.java
+++ Queue/src/br/unicap/edi/ana/Queue.java
@@ -1,4 +1,21 @@
 package br.unicap.edi.ana;
-public class Queue {
+
+public class Queue<T> {
+    private LSENode<T> first;
+    private LSENode<T> last;
+    private int nodes;
+    
+    public boolean isEmpty() {
+        if (this.first == null && this.last == null && this.nodes == 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    public void insertLast(T content){
+
+    } 
+
+    
     
 }
