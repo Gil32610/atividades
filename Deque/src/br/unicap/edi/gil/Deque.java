432787201c5b2ae70a41c83db41bb1bdbe799bf2
GIL <gil.00000032610@unicap.br>
2022-10-14 17:18:51 -0300

    Deque implemented

---
+++ Deque/src/br/unicap/edi/gil/Deque.java
@@ -0,0 +1,71 @@
+package br.unicap.edi.gil;
+
+public class Deque<T> {
+    private LSENode<T> head;
+    private LSENode<T> tail;
+    private int capacity;
+    private int quantity;
+
+    public Deque(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean isEmpty() {
+        if (this.head == null && this.quantity == 0 && this.tail == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public void push(T content) {
+        LSENode current = new LSENode<T>(content);
+        if (this.isEmpty()) {
+            this.head = current;
+            this.tail = current;
+            this.quantity++;
+        } else {
+            current.setNext(this.head);
+            this.head = current;
+            this.quantity++;
+        }
+
+    }
+
+    public T pop() {
+        T content = this.head.getContent();
+        this.head = this.head.getNext();
+        this.quantity--;
+        return content;
+    }
+
+    public void inject(T content) {
+        LSENode current = new LSENode<T>(content);
+        if (this.isEmpty()) {
+            this.head = current;
+            this.tail = current;
+            this.quantity++;
+        } else {
+            this.tail.setNext(current);
+            this.tail = current;
+            quantity++;
+        }
+    }
+
+    public T eject() {
+        T content = this.tail.getContent();
+        LSENode current = this.head;
+        while (current.getNext() != this.tail) {
+            current = current.getNext();
+        }
+        current.setNext(null);
+        this.tail = current;
+        return content;
+    }
+
+    public boolean isFull() {
+        if (this.quantity == this.capacity) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Deque/src/br/unicap/edi/gil/LSENode.java
@@ -0,0 +1,25 @@
+package br.unicap.edi.gil;
+
+    public class LSENode<T> {
+        private T content;
+        private LSENode<T> next;   
+
+
+        
+        public LSENode (T content) {
+            this.content = content;
+        }
+        public void setContent(T content) {
+            this.content = content;
+        }
+        public T getContent() {
+            return this.content;
+        }
+        public void setNext(LSENode<T> novoProx) {
+            this.next = novoProx;
+        }
+        public LSENode<T> getNext () {
+            return this.next;
+        }    
+     
+}
