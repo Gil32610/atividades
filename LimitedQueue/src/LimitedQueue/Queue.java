db22328f0c0ea0df0f469ddfedf9754743107ab4
GIL <gil.00000032610@unicap.br>
2022-10-17 23:13:11 -0300

    RuntimeEXCEPTION

--- LimitedQueue/src/LimitedQueue/Queue.java
+++ LimitedQueue/src/LimitedQueue/Queue.java
@@ -1,61 +1,68 @@
 package LimitedQueue;
-    public class Queue<T> {
-        private LSENode<T> head;
-        private LSENode<T> tail;
-        private int nodes;
-        private int quantity;
-    
-        public Queue(int nodes) {
-            this.nodes = nodes;
+
+import javax.management.RuntimeErrorException;
+
+public class Queue<T> {
+    private LSENode<T> head;
+    private LSENode<T> tail;
+    private int nodes;
+    private int quantity;
+
+    public Queue(int nodes) {
+        this.nodes = nodes;
+    }
+
+    public boolean isEmpty() {
+        if (this.head == null && this.tail == null && this.quantity == 0) {
+            return true;
         }
-    
-        public boolean isEmpty() {
-            if (this.head == null && this.tail == null && this.quantity == 0) {
-                return true;
-            }
-            return false;
-        }
-    
-        public void enQueue(T content) {
-            LSENode<T> node = new LSENode(content);
-            if (this.isEmpty()) {
-                this.head = node;
-                this.tail = node;
-                this.quantity++;
-            } else {
+        return false;
+    }
+
+    public void enQueue(T content) throws RuntimeException {
+        LSENode<T> node = new LSENode(content);
+        if (this.isEmpty()) {
+            this.head = node;
+            this.tail = node;
+            this.quantity++;
+        } else {
+            if (!this.isFull()) {
                 this.tail.setNext(node);
                 this.tail = node;
                 this.quantity++;
+            } else {
+                throw new RuntimeException();
             }
-        }
-    
-        public T deQueue() {
-            T content;
-            if (this.head.getNext() == null) {
-                content = this.head.getContent();
-                this.head = null;
-                this.tail = null;
-                this.quantity = 0;
-                return content;
-    
-            }
-            LSENode<T> current = this.head.getNext();
-            content = this.head.getContent();
-            this.head = current;
-            this.quantity--;
-            return content;
-    
-        }
-    
-        public T head() {
-            T content = this.head.getContent();
-            return content;
-        }
-    
-        public boolean isFull() {
-            if (this.quantity == nodes) {
-                return true;
-            }
-            return false;
         }
     }
+
+    public T deQueue() {
+        T content;
+        if (this.head.getNext() == null) {
+            content = this.head.getContent();
+            this.head = null;
+            this.tail = null;
+            this.quantity = 0;
+            return content;
+
+        }
+        LSENode<T> current = this.head.getNext();
+        content = this.head.getContent();
+        this.head = current;
+        this.quantity--;
+        return content;
+
+    }
+
+    public T head() {
+        T content = this.head.getContent();
+        return content;
+    }
+
+    public boolean isFull() {
+        if (this.quantity == nodes) {
+            return true;
+        }
+        return false;
+    }
+}
